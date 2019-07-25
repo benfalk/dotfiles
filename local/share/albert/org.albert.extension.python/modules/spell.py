b4f746f123890c88f2d0d7e6dbf592a8840b88b3
Benjamin Falk <benjamin.falk@yahoo.com>
2019-07-25 09:52:54 -0400

    Spell Checking has returned!

--- local/share/albert/org.albert.extension.python/modules/spell.py
+++ local/share/albert/org.albert.extension.python/modules/spell.py
@@ -1,40 +1,40 @@
 """
-    Spell Better N00b
+    Spell Better
 """
 from albertv0 import *
 from subprocess import Popen, PIPE
+from os import path
 import re
 
 __iid__ = 'PythonInterface/v0.1'
 __prettyname__ = 'Spell'
 __version__ = '1.0'
-__trigger__ = 'spell '
+__trigger__ = 'sp '
 __author__ = 'Ben Falk'
 __dependencies__ = ['aspell']
-
-def to_clip(str):
-    return ClipAction(str)
-
-def to_suggestions(query):
-    spell = Popen(["aspell", "-a"], stdout=PIPE, stdin=PIPE)
-    output = spell.communicate(input=query)[0].split('\n')[1]
-    maybe_suggestions = re.match('.*:(.*)', output)
-
-    if maybe_suggestions is None:
-        return Item(
-                id=str(query),
-                text="correct",
-                actions=[to_clip(query)]
-                )
-    else:
-        suggestions = maybe_suggestions.groups()[0].split(',')
-        return Item(
-                id=str(query),
-                text="suggestions",
-                actions=map(to_clip, suggestions)
-                )
+__icon__ = path.dirname(__file__) + '/icons/sp.png'
 
 def handleQuery(query):
     if query.isTriggered and len(query.string) < 3:
-        return [Item(id='spell', text='Spell Better')]
-    return to_suggestions(query.string)
+        return [Item(id='spell', icon=__icon__, text='Spell Better')]
+
+    proc = Popen(["aspell", "-a"], stdout=PIPE, stdin=PIPE)
+
+    word = query.string
+
+    output = proc.communicate(input=bytes(word, 'utf8'))[0].decode().split('\n')[1]
+
+    maybe_suggestions = re.match('.*:(.*)', output)
+
+    if maybe_suggestions is None:
+        return Item(id=word, icon=__icon__, text="correct", actions=[to_clip(word)])
+    else:
+        suggestions = maybe_suggestions.groups()[0].split(',')
+        return list(map(to_suggestion, suggestions))
+
+def to_suggestion(word):
+    word = word.strip()
+    return Item(id=word, icon=__icon__, text=word, actions=[to_clip(word)])
+
+def to_clip(word):
+    return ClipAction(text=word, clipboardText=word)
